a0742c50a02deb0493ed0e2259b4d08e26341e4b
Azad Hüseyin Kılıç <azadhuseyinkilic1@gmail.com>
2025-10-16 18:04:23 +0300

    Add GSAP animations and responsive video handling in Hero and Highlights components; update Navbar with navigation links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,27 @@
+import { navLists } from "../constants";
+import { appleImg, bagImg, searchImg } from "../utils";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex screen-max-width w-full">
+        <img src={appleImg} alt="apple" width={14} height={18} />
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+              key={nav}
+            >
+              {nav}
+            </div>
+          ))}
+        </div>
+
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
